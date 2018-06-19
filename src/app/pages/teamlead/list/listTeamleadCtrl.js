94fe3483f8d487d35207dfcd65a70843cc764358
hao.nguyen <haonguyenh2@HarveyNash.vn.local>
2018-06-19 12:55:39 +0700

    create page danh sach don hang

--- src/app/pages/teamlead/list/listTeamleadCtrl.js
+++ src/app/pages/teamlead/list/listTeamleadCtrl.js
@@ -1,19 +1,132 @@
 (function () {
     'use strict';
-  
+
     angular.module('BlurAdmin.pages.teamlead')
         .controller('ListTeamleadCtrl', ListTeamleadCtrl);
-  
+
     /** @ngInject */
     function ListTeamleadCtrl($scope) {
-     var vm = this;
-  
-      vm.personalInfo = {};
-      vm.productInfo = {};
-      vm.shipment = {};
+        $scope.pageSize = 5;
 
+        $scope.listTeamlead = [
+            {
+                orderCode: 'DH test luồng assign converter',
+                orderName: 'Chuyên viên tuyển dụng',
+                teamlead: 'huongtt25',
+                kpi: 1,
+                deadline: '18/01/2018',
+                proposer: 'phuongnt15',
+                linkJD: 'http://google.com'
+            },
+            {
+                orderCode: 'DH test luồng assign converter',
+                orderName: 'Chuyên viên tuyển dụng',
+                teamlead: 'huongtt25',
+                kpi: 1,
+                deadline: '18/01/2018',
+                proposer: 'phuongnt15',
+                linkJD: 'http://google.com'
+            },
+            {
+                orderCode: 'DH test luồng assign converter',
+                orderName: 'Chuyên viên tuyển dụng',
+                teamlead: 'huongtt25',
+                kpi: 1,
+                deadline: '18/01/2018',
+                proposer: 'phuongnt15',
+                linkJD: 'http://google.com'
+            },
+            {
+                orderCode: 'DH test luồng assign converter',
+                orderName: 'Chuyên viên tuyển dụng',
+                teamlead: 'huongtt25',
+                kpi: 1,
+                deadline: '18/01/2018',
+                proposer: 'phuongnt15',
+                linkJD: 'http://google.com'
+            },
+            {
+                orderCode: 'DH test luồng assign converter',
+                orderName: 'Chuyên viên tuyển dụng',
+                teamlead: 'huongtt25',
+                kpi: 1,
+                deadline: '18/01/2018',
+                proposer: 'phuongnt15',
+                linkJD: 'http://google.com'
+            },
+            {
+                orderCode: 'DH test luồng assign converter',
+                orderName: 'Chuyên viên tuyển dụng',
+                teamlead: 'huongtt25',
+                kpi: 1,
+                deadline: '18/01/2018',
+                proposer: 'phuongnt15',
+                linkJD: 'http://google.com'
+            }, {
+                orderCode: 'DH test luồng assign converter',
+                orderName: 'Chuyên viên tuyển dụng',
+                teamlead: 'huongtt25',
+                kpi: 1,
+                deadline: '18/01/2018',
+                proposer: 'phuongnt15',
+                linkJD: 'http://google.com'
+            }
+            , {
+                orderCode: 'DH test luồng assign converter',
+                orderName: 'Chuyên viên tuyển dụng',
+                teamlead: 'huongtt25',
+                kpi: 1,
+                deadline: '18/01/2018',
+                proposer: 'phuongnt15',
+                linkJD: 'http://google.com'
+            }
+            , {
+                orderCode: 'DH test luồng assign converter',
+                orderName: 'Chuyên viên tuyển dụng',
+                teamlead: 'huongtt25',
+                kpi: 1,
+                deadline: '18/01/2018',
+                proposer: 'phuongnt15',
+                linkJD: 'http://google.com'
+            }
+            , {
+                orderCode: 'DH test luồng assign converter',
+                orderName: 'Chuyên viên tuyển dụng',
+                teamlead: 'huongtt25',
+                kpi: 1,
+                deadline: '18/01/2018',
+                proposer: 'phuongnt15',
+                linkJD: 'http://google.com'
+            }
+            , {
+                orderCode: 'DH test luồng assign converter',
+                orderName: 'Chuyên viên tuyển dụng',
+                teamlead: 'huongtt25',
+                kpi: 1,
+                deadline: '18/01/2018',
+                proposer: 'phuongnt15',
+                linkJD: 'http://google.com'
+            }
+            , {
+                orderCode: 'DH test luồng assign converter',
+                orderName: 'Chuyên viên tuyển dụng',
+                teamlead: 'huongtt25',
+                kpi: 1,
+                deadline: '18/01/2018',
+                proposer: 'phuongnt15',
+                linkJD: 'http://google.com'
+            }
+            , {
+                orderCode: 'DH test luồng assign converter',
+                orderName: 'Chuyên viên tuyển dụng',
+                teamlead: 'huongtt25',
+                kpi: 1,
+                deadline: '18/01/2018',
+                proposer: 'phuongnt15',
+                linkJD: 'http://google.com'
+            }
+        ]
     }
-  
-  })();
-  
-  
+
+})();
+
