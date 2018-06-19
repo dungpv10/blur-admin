a63113c2bb701d6d54d766a6c0b6113e9a0d1796
hao.nguyen <haonguyenh2@HarveyNash.vn.local>
2018-06-19 15:50:00 +0700

    create assign people page

--- src/app/pages/teamlead/assignPeople/assignPeopleCtrl.js
+++ src/app/pages/teamlead/assignPeople/assignPeopleCtrl.js
@@ -2,19 +2,160 @@
   'use strict';
 
   angular.module('BlurAdmin.pages.teamlead')
-      .controller('AssignPeopleCtrl', AssignPeopleCtrl);
+    .controller('AssignPeopleCtrl', AssignPeopleCtrl);
 
   /** @ngInject */
   function AssignPeopleCtrl($scope) {
-   var vm = this;
+    $scope.selectedPageSize = {};
+    $scope.pageSize = 5;
 
-    vm.personalInfo = {};
-    vm.productInfo = {};
-    vm.shipment = {};
+    $scope.listAssignPeople = [
+      {
+        id: 1,
+        fullName: 'Tran Tuan Anh',
+        email: 'trantuananh@gmail.com',
+        linkCV: 'http://drive.google.com/trantuananh.cv',
+        status: 0,
+        result: 1,
+        proposer: 'phuongnt15'
+      },
+      {
+        id: 2,
+        fullName: 'Tran Tuan Anh',
+        email: 'trantuananh@gmail.com',
+        linkCV: 'http://drive.google.com/trantuananh.cv',
+        status: 0,
+        result: 1,
+        proposer: 'phuongnt15'
+      },
+      {
+        id: 3,
+        fullName: 'Tran Tuan Anh',
+        email: 'trantuananh@gmail.com',
+        linkCV: 'http://drive.google.com/trantuananh.cv',
+        status: 0,
+        result: 1,
+        proposer: 'phuongnt15'
+      },
+      {
+        id: 4,
+        fullName: 'Tran Tuan Anh',
+        email: 'trantuananh@gmail.com',
+        linkCV: 'http://drive.google.com/trantuananh.cv',
+        status: 0,
+        result: 1,
+        proposer: 'phuongnt15'
+      },
+      {
+        id: 5,
+        fullName: 'Tran Tuan Anh',
+        email: 'trantuananh@gmail.com',
+        linkCV: 'http://drive.google.com/trantuananh.cv',
+        status: 0,
+        result: 1,
+        proposer: 'phuongnt15'
+      },
+      {
+        id: 6,
+        fullName: 'Tran Tuan Anh',
+        email: 'trantuananh@gmail.com',
+        linkCV: 'http://drive.google.com/trantuananh.cv',
+        status: 0,
+        result: 1,
+        proposer: 'phuongnt15'
+      }, 
+      {
+        id: 7,
+        fullName: 'Tran Tuan Anh',
+        email: 'trantuananh@gmail.com',
+        linkCV: 'http://drive.google.com/trantuananh.cv',
+        status: 0,
+        result: 1,
+        proposer: 'phuongnt15'
+      }
+      , {
+        id: 8,
+        fullName: 'Tran Tuan Anh',
+        email: 'trantuananh@gmail.com',
+        linkCV: 'http://drive.google.com/trantuananh.cv',
+        status: 0,
+        result: 1,
+        proposer: 'phuongnt15'
+      }
+      , 
+      {
+        id: 9,
+        fullName: 'Tran Tuan Anh',
+        email: 'trantuananh@gmail.com',
+        linkCV: 'http://drive.google.com/trantuananh.cv',
+        status: 0,
+        result: 1,
+        proposer: 'phuongnt15'
+      }
+      , 
+      {
+        id: 10,
+        fullName: 'Tran Tuan Anh',
+        email: 'trantuananh@gmail.com',
+        linkCV: 'http://drive.google.com/trantuananh.cv',
+        status: 0,
+        result: 1,
+        proposer: 'phuongnt15'
+      }
+      , {
+        id: 11,
+        fullName: 'Tran Tuan Anh',
+        email: 'trantuananh@gmail.com',
+        linkCV: 'http://drive.google.com/trantuananh.cv',
+        status: 0,
+        result: 1,
+        proposer: 'phuongnt15'
+      }
+      , {
+        id: 12,
+        fullName: 'Tran Tuan Anh',
+        email: 'trantuananh@gmail.com',
+        linkCV: 'http://drive.google.com/trantuananh.cv',
+        status: 0,
+        result: 1,
+        proposer: 'phuongnt15'
+      }
+      , {
+        id: 13,
+        fullName: 'Tran Tuan Anh',
+        email: 'trantuananh@gmail.com',
+        linkCV: 'http://drive.google.com/trantuananh.cv',
+        status: 0,
+        result: 1,
+        proposer: 'phuongnt15'
+      }
+    ];
 
-    vm.arePersonalInfoPasswordsEqual = function () {
-      return vm.personalInfo.confirmPassword && vm.personalInfo.password == vm.personalInfo.confirmPassword;
-    };
+    $scope.sizes = [
+      {
+        value: 5,
+        text: '5'
+      },
+      {
+        value: 10,
+        text: '10'
+      },
+      {
+        value: 20,
+        text: '20'
+      },
+      {
+        value: 25,
+        text: '25'
+      }
+    ];
+
+    $scope.selectedPageSize = $scope.sizes[0];
+
+    $scope.changePagesize = function () {
+      console.log($scope.selectedPageSize)
+      $scope.pageSize = $scope.selectedPageSize.value;
+    }
   }
 
 })();
