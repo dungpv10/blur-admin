67cbe58b1554cc0e8a72d90634d21a6d0dd32884
Pham Dung <dungpv10@gmail.com>
2018-06-20 17:30:46 +0700

    add data for contact list

--- src/app/pages/contact/list/listContactCtrl.js
+++ src/app/pages/contact/list/listContactCtrl.js
@@ -10,124 +10,32 @@
         $scope.selectedDisplayColumn = {};
         $scope.pageSize = 5;
 
-        $scope.listTeamlead = [
-            {
-                orderCode: 'DH test luồng assign converter',
-                orderName: 'Chuyên viên tuyển dụng',
-                teamlead: 'huongtt25',
-                kpi: 1,
-                deadline: '18/01/2018',
-                proposer: 'phuongnt15',
-                linkJD: 'http://google.com'
-            },
-            {
-                orderCode: 'DH test luồng assign converter',
-                orderName: 'Chuyên viên tuyển dụng',
-                teamlead: 'huongtt25',
-                kpi: 1,
-                deadline: '18/01/2018',
-                proposer: 'phuongnt15',
-                linkJD: 'http://google.com'
-            },
-            {
-                orderCode: 'DH test luồng assign converter',
-                orderName: 'Chuyên viên tuyển dụng',
-                teamlead: 'huongtt25',
-                kpi: 1,
-                deadline: '18/01/2018',
-                proposer: 'phuongnt15',
-                linkJD: 'http://google.com'
-            },
-            {
-                orderCode: 'DH test luồng assign converter',
-                orderName: 'Chuyên viên tuyển dụng',
-                teamlead: 'huongtt25',
-                kpi: 1,
-                deadline: '18/01/2018',
-                proposer: 'phuongnt15',
-                linkJD: 'http://google.com'
-            },
-            {
-                orderCode: 'DH test luồng assign converter',
-                orderName: 'Chuyên viên tuyển dụng',
-                teamlead: 'huongtt25',
-                kpi: 1,
-                deadline: '18/01/2018',
-                proposer: 'phuongnt15',
-                linkJD: 'http://google.com'
-            },
-            {
-                orderCode: 'DH test luồng assign converter',
-                orderName: 'Chuyên viên tuyển dụng',
-                teamlead: 'huongtt25',
-                kpi: 1,
-                deadline: '18/01/2018',
-                proposer: 'phuongnt15',
-                linkJD: 'http://google.com'
-            }, {
-                orderCode: 'DH test luồng assign converter',
-                orderName: 'Chuyên viên tuyển dụng',
-                teamlead: 'huongtt25',
-                kpi: 1,
-                deadline: '18/01/2018',
-                proposer: 'phuongnt15',
-                linkJD: 'http://google.com'
-            }
-            , {
-                orderCode: 'DH test luồng assign converter',
-                orderName: 'Chuyên viên tuyển dụng',
-                teamlead: 'huongtt25',
-                kpi: 1,
-                deadline: '18/01/2018',
-                proposer: 'phuongnt15',
-                linkJD: 'http://google.com'
-            }
-            , {
-                orderCode: 'DH test luồng assign converter',
-                orderName: 'Chuyên viên tuyển dụng',
-                teamlead: 'huongtt25',
-                kpi: 1,
-                deadline: '18/01/2018',
-                proposer: 'phuongnt15',
-                linkJD: 'http://google.com'
-            }
-            , {
-                orderCode: 'DH test luồng assign converter',
-                orderName: 'Chuyên viên tuyển dụng',
-                teamlead: 'huongtt25',
-                kpi: 1,
-                deadline: '18/01/2018',
-                proposer: 'phuongnt15',
-                linkJD: 'http://google.com'
-            }
-            , {
-                orderCode: 'DH test luồng assign converter',
-                orderName: 'Chuyên viên tuyển dụng',
-                teamlead: 'huongtt25',
-                kpi: 1,
-                deadline: '18/01/2018',
-                proposer: 'phuongnt15',
-                linkJD: 'http://google.com'
-            }
-            , {
-                orderCode: 'DH test luồng assign converter',
-                orderName: 'Chuyên viên tuyển dụng',
-                teamlead: 'huongtt25',
-                kpi: 1,
-                deadline: '18/01/2018',
-                proposer: 'phuongnt15',
-                linkJD: 'http://google.com'
-            }
-            , {
-                orderCode: 'DH test luồng assign converter',
-                orderName: 'Chuyên viên tuyển dụng',
-                teamlead: 'huongtt25',
-                kpi: 1,
-                deadline: '18/01/2018',
-                proposer: 'phuongnt15',
-                linkJD: 'http://google.com'
-            }
-        ];
+        $scope.listTeamlead = [];
+        for(let i = 0; i < 25; i++){
+            $scope.listTeamlead.push({
+                findCVDay: '15/12/2018',
+                userName: 'huongtt25',
+                email: 'huongtt25@gmail.com',
+                numberPhone: '0989456231',
+                CVLink: 'https://google.com',
+                source: 'source',
+                order: Math.round(Math.random()),
+                ttpvChamCVNgayGoi : '15/12/2018',
+                ttpvChamCVKetQua : 'trúng tuyển',
+                ttpvMoiUngTuyenKetQua : 'trúng tuyển',
+                ttpvMoiUngTuyenTrangThai : 'trúng tuyển',
+                ttpvOrientationKetQua : 'Trúng tuyển',
+                ttpvOrientationNgayPhongVan : 'Trúng tuyển',
+                ttpvPVCMNgayPhongVan : '15/12/2018',
+                ttpvPVVHKetQua : 'Trúng tuyển',
+                ttpvPVVHNgayPhongVan : '15/12/2018',
+                ttpvGTATNgayPhongVan : '15/12/2018',
+                ttpvGTATKetQua : 'Trúng tuyển',
+                ttpvOfferKetQua : 'Trúng tuyển',
+                ttpvRankingKetQua : 'Trúng tuyển'
+            });
+        }
+
 
         $scope.sizes = teamleadService.getListPageSize();
         $scope.listDisplayColumns = teamleadService.getListDisplayColumns();
