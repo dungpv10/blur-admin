35402cfd7cad4053af3b47f3257d271148f5d8b7
hao.nguyen <haonguyenh2@HarveyNash.vn.local>
2018-06-21 15:34:15 +0700

    fix pagination

--- src/app/pages/teamlead/assignConvert/assignConvertCtrl.js
+++ src/app/pages/teamlead/assignConvert/assignConvertCtrl.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('BlurAdmin.pages.teamlead')
-      .controller('AssignConvertCtrl', AssignConvertCtrl);
+    .controller('AssignConvertCtrl', AssignConvertCtrl);
 
   /** @ngInject */
   function AssignConvertCtrl($scope, teamleadService) {
@@ -10,131 +10,24 @@
     $scope.pageSize = 5;
     $scope.assignLabel = 'Converter';
 
-    $scope.listConverter = [
-      {
-        id: 1,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      },
-      {
-        id: 2,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      },
-      {
-        id: 3,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      },
-      {
-        id: 4,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      },
-      {
-        id: 5,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      },
-      {
-        id: 6,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }, 
-      {
-        id: 7,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-      , {
-        id: 8,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-      , 
-      {
-        id: 9,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-      , 
-      {
-        id: 10,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-      , {
-        id: 11,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-      , {
-        id: 12,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-      , {
-        id: 13,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-    ];
+    $scope.listConverter = [];
+
+    for (var i = 0; i < 28; ++i) {
+      $scope.listConverter.push(
+        {
+          id: i,
+          fullName: 'Tran Tuan Anh',
+          email: 'trantuananh@gmail.com',
+          linkCV: 'http://drive.google.com/trantuananh.cv',
+          status: 0,
+          result: 1,
+          proposer: 'phuongnt15'
+        });
+    }
 
     $scope.sizes = $scope.sizes = teamleadService.getListPageSize();
     $scope.listDisplayColumns = teamleadService.getListDisplayColumns();
-        $scope.listDisplayColumns.unshift({value: '', text: 'Full Name, Phone, Email, Source, Load CV, Người chấm'});
+    $scope.listDisplayColumns.unshift({ value: '', text: 'Full Name, Phone, Email, Source, Load CV, Người chấm' });
 
     $scope.selectedPageSize = $scope.sizes[0];
 
--- src/app/pages/teamlead/assignPeople/assignPeopleCtrl.js
+++ src/app/pages/teamlead/assignPeople/assignPeopleCtrl.js
@@ -9,132 +9,24 @@
     $scope.selectedPageSize = {};
     $scope.pageSize = 5;
     $scope.assignLabel = 'Người chấm';
+    $scope.listAssignPeople = [];
 
-    $scope.listAssignPeople = [
-      {
-        id: 1,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      },
-      {
-        id: 2,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      },
-      {
-        id: 3,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      },
-      {
-        id: 4,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      },
-      {
-        id: 5,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      },
-      {
-        id: 6,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }, 
-      {
-        id: 7,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-      , {
-        id: 8,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-      , 
-      {
-        id: 9,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-      , 
-      {
-        id: 10,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-      , {
-        id: 11,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-      , {
-        id: 12,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-      , {
-        id: 13,
-        fullName: 'Tran Tuan Anh',
-        email: 'trantuananh@gmail.com',
-        linkCV: 'http://drive.google.com/trantuananh.cv',
-        status: 0,
-        result: 1,
-        proposer: 'phuongnt15'
-      }
-    ];
+    for (var i = 0; i < 28; ++i) {
+      $scope.listAssignPeople.push(
+        {
+          id: i,
+          fullName: 'Tran Tuan Anh',
+          email: 'trantuananh@gmail.com',
+          linkCV: 'http://drive.google.com/trantuananh.cv',
+          status: 0,
+          result: 1,
+          proposer: 'phuongnt15'
+        });
+    }
 
     $scope.sizes = $scope.sizes = teamleadService.getListPageSize();
     $scope.listDisplayColumns = teamleadService.getListDisplayColumns();
-        $scope.listDisplayColumns.unshift({value: '', text: 'Full Name, Phone, Email, Source, Load CV, Người chấm'});
+    $scope.listDisplayColumns.unshift({ value: '', text: 'Full Name, Phone, Email, Source, Load CV, Người chấm' });
 
     $scope.selectedPageSize = $scope.sizes[0];
 
